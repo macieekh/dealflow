7f94858c0e67778269ae805971885300deb67819
Maciek <maciej.harazinski@gmail.com>
2013-12-21 13:32:20 +0100

    top menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Dealflow::Application.routes.draw do
-  get "pages/home"
+  root "pages#home"
+  get "about" => "pages#about" # Creates about_path
+  # get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
