8060cedbcf6690a061aafcb5347d3f1c3239e7ac
Maciek <maciej.harazinski@gmail.com>
2013-12-21 12:55:19 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dealflow::Application.config.secret_key_base = 'a4779624905f57c9c4461fe497d609dd76b851ffa56e20278b0c4c887ee639b453781c00dd6d5287820cbae1bca1818218d55ed269436de71f78294ae4609a6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dealflow::Application.config.session_store :cookie_store, key: '_dealflow_session'
