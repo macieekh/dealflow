b4f3a874b55a898a449ef6bd0398f7a2e3d9cbef
Maciek <maciej.harazinski@gmail.com>
2013-12-22 00:11:53 +0100

    devise installed with devise:views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Dealflow::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku - update the 'localost'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
